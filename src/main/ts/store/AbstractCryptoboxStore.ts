a847b9c60b3f2b3a5ae77855ab378779bf6a3355
Benny Neugebauer <bn@bennyn.de>
2017-03-23 14:49:11 +0100

    Implemented basic functions

--- src/main/ts/store/AbstractCryptoboxStore.ts
+++ src/main/ts/store/AbstractCryptoboxStore.ts
@@ -1,6 +1,7 @@
 import * as Proteus from "wire-webapp-proteus";
 import {CryptoboxStore} from "./CryptoboxStore";
 import {SerialisedRecord} from "./SerialisedRecord";
+import {RecordNotFoundError} from "./RecordNotFoundError";
 
 export abstract class AbstractCryptoboxStore implements CryptoboxStore {
   static get KEYS() {
@@ -17,18 +18,36 @@ export abstract class AbstractCryptoboxStore implements CryptoboxStore {
     };
   }
 
-  abstract create(store_name: string, primary_key: string, entity: SerialisedRecord);
+  abstract create(store_name: string, primary_key: string, entity: SerialisedRecord): Promise<string>;
 
-  delete_all(): Promise<boolean> {
-    return undefined;
-  }
+  abstract read(store_name: string, primary_key: string): Promise<Object>;
+
+  abstract delete(store_name: string, primary_key: string): Promise<string>;
+
+  abstract delete_all(): Promise<boolean>;
 
   delete_prekey(prekey_id: number): Promise<number> {
-    return undefined;
+    return this.delete(AbstractCryptoboxStore.STORES.PRE_KEYS, prekey_id.toString())
+      .then(() => {
+        return prekey_id;
+      });
   }
 
   load_identity(): Promise<Proteus.keys.IdentityKeyPair> {
-    return undefined;
+    return new Promise((resolve, reject) => {
+      this.read(AbstractCryptoboxStore.STORES.LOCAL_IDENTITY, AbstractCryptoboxStore.KEYS.LOCAL_IDENTITY)
+        .then((record: SerialisedRecord) => {
+          const identity: Proteus.keys.IdentityKeyPair = Proteus.keys.IdentityKeyPair.deserialise(record.serialised);
+          resolve(identity);
+        })
+        .catch(function (error: Error) {
+          if (error instanceof RecordNotFoundError) {
+            resolve(undefined);
+          } else {
+            reject(error);
+          }
+        });
+    });
   }
 
   load_prekey(prekey_id: number): Promise<Proteus.keys.PreKey> {
@@ -49,7 +68,12 @@ export abstract class AbstractCryptoboxStore implements CryptoboxStore {
   }
 
   save_prekey(pre_key: Proteus.keys.PreKey): Promise<Proteus.keys.PreKey> {
-    return undefined;
+    const payload: SerialisedRecord = new SerialisedRecord(pre_key.serialise(), pre_key.key_id.toString());
+
+    return this.create(AbstractCryptoboxStore.STORES.PRE_KEYS, payload.id, payload)
+      .then(() => {
+        return pre_key;
+      });
   }
 
   save_prekeys(pre_keys: Array<Proteus.keys.PreKey>): Promise<Array<Proteus.keys.PreKey>> {
--- src/main/ts/store/File.ts
+++ src/main/ts/store/File.ts
@@ -15,8 +15,8 @@ export default class File extends AbstractCryptoboxStore {
   }
 
   create(store_name: string, primary_key: string, entity: SerialisedRecord) {
-    this.logger.log(`Saving record "${primary_key}" in directory "${store_name}"...`, entity);
-    const file: string = path.normalize(`${this.storagePath}/${store_name}/${primary_key}.txt`);
+    this.logger.log(`Creating record "${primary_key}" in directory "${store_name}"...`, entity);
+    const file: string = path.normalize(`${this.storagePath}/${store_name}/${primary_key}.json`);
 
     return new Promise((resolve, reject) => {
       fs.writeFile(file, entity.serialised, {encoding: "utf8", flag: "w+"}, (error) => {
@@ -25,6 +25,62 @@ export default class File extends AbstractCryptoboxStore {
         } else {
           return resolve(file);
         }
+      });
+    });
+  }
+
+  read(store_name: string, primary_key: string): Promise<Object> {
+    this.logger.log(`Reading record with primary key "${primary_key}" from directory "${store_name}"...`);
+    const file: string = path.normalize(`${this.storagePath}/${store_name}/${primary_key}.json`);
+
+    return new Promise((resolve, reject) => {
+      fs.readFile(file, {encoding: 'utf8', flag: 'r'}, function (error, data) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(JSON.parse(data));
+        }
+      });
+    });
+  }
+
+  delete(store_name: string, primary_key: string): Promise<string> {
+    this.logger.log(`Deleting record with primary key "${primary_key}" in directory "${store_name}"...`);
+    const file: string = path.normalize(`${this.storagePath}/${store_name}/${primary_key}.json`);
+
+    return new Promise((resolve, reject) => {
+      fs.unlink(file, (error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(primary_key);
+        }
+      });
+    });
+  }
+
+  delete_all(): Promise<boolean> {
+    const dir = this.storagePath;
+    return new Promise((resolve, reject) => {
+      fs.access(dir, error => {
+        if (error) {
+          return reject(error);
+        }
+        fs.readdir(dir, (error, files) => {
+          if (error) {
+            return reject(error);
+          }
+          Promise.all(files.map(function (file) {
+            return this.delete(dir, file);
+          })).then(() => {
+            fs.rmdir(dir, error => {
+              if (error) {
+                return reject(error);
+              }
+              resolve();
+            });
+          }).catch(reject);
+        });
       });
     });
   }
