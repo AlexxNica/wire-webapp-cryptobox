42a68be93855bb400a7fcd0d4592a4b8ce485433
Benny Neugebauer <bn@bennyn.de>
2017-03-29 15:13:59 +0200

    Reverted test

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,19 @@
 var cryptobox = require('./commonjs/wire-webapp-cryptobox');
 var Logdown = require('logdown');
 
-var fileStore = new cryptobox.store.File();
-fileStore.init(__dirname + '/benny')
-  .then((fileSto) => {
-    return fileSto.delete_all();
-  });
+var logger = new Logdown({prefix: 'Demo', alignOutput: true});
+logger.log(`Testing Cryptobox v${cryptobox.Cryptobox.prototype.VERSION}`);
 
+var store = new cryptobox.store.Cache();
+var box = new cryptobox.Cryptobox(store, 5);
+
+box.init()
+  .then(function() {
+    var fingerprint = box.identity.public_key.fingerprint();
+    console.log('Public Fingerprint', fingerprint);
+    process.exit(0);
+  })
+  .catch(function(error) {
+    console.log('Self test broken: ' + error.message + ' (' + error.stack + ')');
+    process.exit(1);
+  });
